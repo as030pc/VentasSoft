7b4895ad6e64d59fda3ffb25ac7ab95a970720f6
Andres Palma <palma.castaneda030@gmail.com>
2021-09-29 20:32:28 -0500

    Otras partes importantes de la configuracion inicial

---
+++ venta-soft/src/pages/MProductosTabla.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/styleMProductos.css"
+
+
+const MProductosTabla = () => {
+    return (
+        <div className = "div">
+            html
+        </div>
+    )
+}
+
+export default MProductosTabla
