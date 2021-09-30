b61740ed9f96bbf5dc191fe9d8025493328ea965
Andres Palma <palma.castaneda030@gmail.com>
2021-09-29 19:57:30 -0500

    Estructura base

---
+++ venta-soft/src/components/Informacion.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Informacion = () => {
+    return (
+        <div>
+            <h1> Presentacion de los integrantes de VentaSoft</h1>
+            <div>
+                <h1> Nombre: Paola Medina  </h1>
+                <p> Rol: Desarrolladora</p>
+            </div>
+            <div>
+                <h1>Nombre: kevin tejada</h1>
+                <p> Rol:administrador base de datos</p>
+            </div>
+            <div>
+                <h1>Nombre: Mabel Diaz</h1>
+                <p> Rol: Scrum Master </p>
+            </div>
+            <div>
+                <h1>Nombre: Andres Palma</h1>
+                <p> Rol: Product Owner</p>
+            </div>
+            <div>
+                <h1>Nombre: César Gómez</h1>
+                <p>hola, mi  rol es Analista</p>
+            </div>
+            <div>
+                <h1>Nombre: kevin tejada</h1>
+                <p> Rol:administrador base de datos</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Informacion
