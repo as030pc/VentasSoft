3f59cff7929e5041d6ef0062abc9f76a481a0d28
polamedina <polamedina@hotmail.com>
2021-09-29 20:58:46 -0500

    Estructura de trabajo2

--- venta-soft/src/index.js
+++ venta-soft/src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
 import MVentasVentaNueva from './pages/MVentasVentaNueva';
 import MVentasLista from './pages/MVentasLista';
 ReactDOM.render(<> <MVentasVentaNueva/><MVentasLista/> </>
 ,
-=======
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
->>>>>>> origin/Sprint2React
   document.getElementById('root')
 );
